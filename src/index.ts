9a308f4dc94ec733ae395cf72f48335bd909ea9d
gongjian <gongjian@zycfcinc.com>
2019-12-17 20:34:07 +0800

    feat: 编写transformRequest函数

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from './types/index';
 import xhr from './xhr';
 import { buildUrl } from './helps/url';
+import { transformRequest } from './helps/data';
 
 function axios(config: AxiosRequestConfig): void {
   processConfig(config);
@@ -9,11 +10,16 @@ function axios(config: AxiosRequestConfig): void {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config);
+  config.data = transformRequestData(config);
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config;
   return buildUrl(url, params);
+}
+
+function transformRequestData(config: AxiosRequestConfig): void {
+  return transformRequest(config.data);
 }
 
 export default axios
