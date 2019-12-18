35c38c83e4de9afc0aa71477d28d3110544c8305
gongjian <gongjian@zycfcinc.com>
2019-12-18 15:53:57 +0800

    feat: axios基础功能实现

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import { AxiosRequestConfig } from './types/index';
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types/index';
 import xhr from './xhr';
 import { buildUrl } from './helps/url';
 import { transformRequest } from './helps/data';
+import { processHeaders } from './helps/headers';
+import { transformResponse } from './helps/data';
 
-function axios(config: AxiosRequestConfig): void {
+function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config);
-  xhr(config);
+  return xhr(config).then(res => {
+    return transformResponseData(res);
+  })
 }
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config);
+  config.headers = transformHeaders(config);
   config.data = transformRequestData(config);
 }
 
@@ -20,6 +25,16 @@ function transformUrl(config: AxiosRequestConfig): string {
 
 function transformRequestData(config: AxiosRequestConfig): void {
   return transformRequest(config.data);
+}
+
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers, data } = config;
+  processHeaders(headers, data);
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res;
 }
 
 export default axios
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,10 +1,44 @@
-import { AxiosRequestConfig } from './types/index';
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types/index';
+import { parseHeaders } from './helps/headers';
 
-export default function xhr(config: AxiosRequestConfig): void {
-  const { data = null, url, method = 'get' } = config;
-  const request = new XMLHttpRequest();
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise((resolve) => {
+    const { data = null, url, method = 'get', headers, responseType } = config;
+    const request = new XMLHttpRequest();
+  
+    if (responseType) {
+      request.responseType = responseType;
+    }
 
-  request.open(method.toUpperCase(), url, true);
+    request.open(method.toUpperCase(), url, true);
+    
+    request.onreadystatechange = function handleLoad () {
+      if (request.readyState !== 4) {
+        return;
+      }
 
-  request.send(data);
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders());
+      const responseData = responseType !== 'text' ? request.response : request.responseText;
+      const { status, statusText } = request;
+      const response: AxiosResponse = {
+        data: responseData,
+        status,
+        statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+      resolve(response);
+    }
+
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name]);
+      }
+    })
+  
+    request.send(data);
+  })
 }
