60c84606c74e6de863c7f477198bb1733927a5d0
gongjian <gongjian@zycfcinc.com>
2020-01-16 16:53:12 +0800

    feat: 混合对象、函数重载、响应数据支持泛型

--- src/axios.ts
+++ src/axios.ts
@@ -1,40 +1,16 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types/index';
-import xhr from './xhr';
-import { buildUrl } from './helps/url';
-import { transformRequest } from './helps/data';
-import { processHeaders } from './helps/headers';
-import { transformResponse } from './helps/data';
+import { AxiosInstence } from './types/index';
+import Axios from './core/Axios';
+import { extend } from './helps/utils';
 
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config);
-  return xhr(config).then(res => {
-    return transformResponseData(res);
-  })
+function createInstance(): AxiosInstence {
+  const context = new Axios();
+  const instance = Axios.prototype.request.bind(context);
+
+  extend(instance, context);
+
+  return instance as AxiosInstence;
 }
 
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformUrl(config);
-  config.headers = transformHeaders(config);
-  config.data = transformRequestData(config);
-}
+const axios = createInstance();
 
-function transformUrl(config: AxiosRequestConfig): string {
-  const { url, params } = config;
-  return buildUrl(url!, params);
-}
-
-function transformRequestData(config: AxiosRequestConfig): void {
-  return transformRequest(config.data);
-}
-
-function transformHeaders(config: AxiosRequestConfig) {
-  const { headers, data } = config;
-  processHeaders(headers, data);
-}
-
-function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
-  return res;
-}
-
-export default axios
+export default axios;
